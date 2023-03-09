9d4293e41f6850c708271c478036731661eb8fca
Quentin Wouters <q.wouters16@gmail.com>
2023-03-09 17:38:00 +0100

    first API/predict2

--- who_dis/api/fast.py
+++ who_dis/api/fast.py
@@ -1,11 +1,6 @@
 from fastapi import FastAPI
 import requests
 
-
-# import os
-# path = os.path.dirname(os.getcwd())
-# path_abs = os.path.join(path,'raw_data/who_dis/api')
-
 ###################### / ######################
 
 app = FastAPI()
@@ -16,16 +11,6 @@ def index():
     return {'Speaker Recogn API test': True}
 
 ###################### /predict ######################
-
-# url = 'http://localhost:8000/predict'
-
-# params = {
-#     'day_of_week': 0, # 0 for Sunday, 1 for Monday, ...
-#     'time': '14:00'
-# }
-
-# response = requests.get(url, params=params)
-# response.json() #=> {wait: 64}
 
 @app.get('/predict')
 def predict(day_of_week, time):
