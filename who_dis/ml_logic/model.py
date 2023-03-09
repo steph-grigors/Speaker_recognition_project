a20a7e75a44e25e3494a1d297861cfb1a0c4f5fb
Yann Laroche <yelaroche@gmail.com>
2023-03-09 16:21:10 +0100

    Added the initializer for the base Simple NN model in model.py.

--- who_dis/ml_logic/model.py
+++ who_dis/ml_logic/model.py
@@ -2,9 +2,9 @@ from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Conv2D, Dense, Flatten
 from tensorflow.keras.metrics import Recall, Precision
 
-def init_basemodel():
+def init_baseCNN():
     '''
-    This function takes has no parameters. It returns an uncompiled baseline model that has the following structure:
+    This function takes has no parameters. It returns an uncompiled baseline CNN model that has the following structure:
     - 1 Conv2D layer with 5 channels, kernal shape (3,3), activation "relu"
     - 1 Flatten layer
     - 1 Dense output layer with 18 neurons, activation "softmax"
@@ -15,7 +15,18 @@ def init_basemodel():
     model.add(Dense(18, activation='softmax'))
     return model
 
-def compile_model(model):
+def init_baseNN():
+    '''
+    This function takes has no parameters. It returns an uncompiled baseline NN model that has the following structure:
+    - 1 Dense layer with 50 neurons, "relu" activation, and input_dim=40
+    - 1 Dense output layer with 18 neurons, activation "softmax"
+    '''
+    model = Sequential()
+    model.add(Dense(50,activation='relu',input_dim=40))
+    model.add(Dense(18,activation='softmax'))
+    return model
+
+def basic_compiler(model):
     '''
     This function takes in a model, compiles it and returns the compiled model.
     Compiler parameters:
